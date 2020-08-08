a725b9cdd00cede03bd648766d3d98f699acc412
Andrew Collier <atc3598@icloud.com>
2020-08-08 13:37:47 -0400

    added parsing and middleware

--- server.js
+++ server.js
@@ -5,5 +5,9 @@ const htmlRoutes = require("./routes/htmlRoutes");
 const app = express();
 const PORT = process.env.PORT || 3000;
 //parsing and middleware
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 //start server
