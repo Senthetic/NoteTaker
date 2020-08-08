f02551150d8facd82e71cbdfe3c2097e9cac5600
Andrew Collier <atc3598@icloud.com>
2020-08-08 14:01:45 -0400

    added store class and async functions

--- db/store.js
+++ db/store.js
@@ -1,2 +1,9 @@
 const util = require("util");
 const fs = require("fs");
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class Store {
+    
+}
