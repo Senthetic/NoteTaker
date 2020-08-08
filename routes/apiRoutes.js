c5a4202f98463fd817dd68d5269419de3052dcf9
Andrew Collier <atc3598@icloud.com>
2020-08-08 13:52:05 -0400

    successfully added a get route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,12 @@
 const router = require("express").Router();
 const store = require("../db/store");
+
+//Get all notes
+router.get("/notes", (req, res) => {
+    store
+      .getNotes()
+      .then((notes) => res.json(notes))
+      .catch((err) => res.status(500).json(err));
+  });
+
+  
