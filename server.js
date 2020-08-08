5b84c99d9f146b8d3caed98e2700dbdbf398cead
Andrew Collier <atc3598@icloud.com>
2020-08-08 13:37:23 -0400

    added comments and created port and started app

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 const express = require("express");
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
+//create port and start app;
+const app = express();
+const PORT = process.env.PORT || 3000;
+//parsing and middleware
+
+//start server
