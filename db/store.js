05f44af26688a3e281a8823590344b78f2ff8c27
Andrew Collier <atc3598@icloud.com>
2020-08-08 14:02:51 -0400

    added getNotes function

--- db/store.js
+++ db/store.js
@@ -12,4 +12,18 @@ class Store {
       write(note) {
         return writeFileAsync("db/db.json", JSON.stringify(note));
       }
+      getNotes() {
+        return this.read().then((notes) => {
+          let parsedNotes;
+    
+          // If notes isn't an array or can't be turned into one, send back a new empty array
+          try {
+            parsedNotes = [].concat(JSON.parse(notes));
+          } catch (err) {
+            parsedNotes = [];
+          }
+    
+          return parsedNotes;
+        });
+      }
 }
