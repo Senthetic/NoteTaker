52b6e2fe771151d0c223127ebd80a3dd6f0af51e
Andrew Collier <atc3598@icloud.com>
2020-08-08 14:07:41 -0400

    added removeNote function

--- db/store.js
+++ db/store.js
@@ -4,6 +4,9 @@ const fs = require("fs");
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
+//package that generates unique ids
+const uuidv1 = require("uuid/v1");
+
 class Store {
     read() {
         return readFileAsync("db/db.json", "utf8");
@@ -15,8 +18,6 @@ class Store {
       getNotes() {
         return this.read().then((notes) => {
           let parsedNotes;
-    
-          // If notes isn't an array or can't be turned into one, send back a new empty array
           try {
             parsedNotes = [].concat(JSON.parse(notes));
           } catch (err) {
@@ -33,13 +34,20 @@ class Store {
           throw new Error("Note 'title' and 'text' cannot be blank");
         }
     
-        // Add a unique id to the note using uuid package
+        // Add generated id
         const newNote = { title, text, id: uuidv1() };
     
-        // Get all notes, add the new note, write all the updated notes, return the newNote
         return this.getNotes()
           .then((notes) => [...notes, newNote])
           .then((updatedNotes) => this.write(updatedNotes))
           .then(() => newNote);
       }
+      removeNote(id) {
+        // Get all notes, remove the note with the given id, write the filtered notes
+        return this.getNotes()
+          .then((notes) => notes.filter((note) => note.id !== id))
+          .then((filteredNotes) => this.write(filteredNotes));
+      }
 }
+
+module.exports = new Store();
