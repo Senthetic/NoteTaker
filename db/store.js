9ecdda44e023c44121a48de5bff8992865a9ed6e
Andrew Collier <atc3598@icloud.com>
2020-08-08 14:04:29 -0400

    added addNote function and implemented unique id package

--- db/store.js
+++ db/store.js
@@ -26,4 +26,20 @@ class Store {
           return parsedNotes;
         });
       }
+      addNote(note) {
+        const { title, text } = note;
+    
+        if (!title || !text) {
+          throw new Error("Note 'title' and 'text' cannot be blank");
+        }
+    
+        // Add a unique id to the note using uuid package
+        const newNote = { title, text, id: uuidv1() };
+    
+        // Get all notes, add the new note, write all the updated notes, return the newNote
+        return this.getNotes()
+          .then((notes) => [...notes, newNote])
+          .then((updatedNotes) => this.write(updatedNotes))
+          .then(() => newNote);
+      }
 }
