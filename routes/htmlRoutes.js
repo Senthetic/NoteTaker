6ac2082008eeccdca02ae62460ab360df0d5f5c9
Andrew Collier <atc3598@icloud.com>
2020-08-08 13:55:53 -0400

    added way to send note.html file

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,2 +1,8 @@
 const path = require("path");
 const router = require("express").Router();
+
+//get notes.html file
+router.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
+
