d0ce5ce237ef64eeb085826bf07840b550057d10
Andrew Collier <atc3598@icloud.com>
2020-08-08 14:56:02 -0400

    Finishing touches

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,26 +3,26 @@ const store = require("../db/store");
 
 //Get all notes
 router.get("/notes", (req, res) => {
-    store
-      .getNotes()
-      .then((notes) => res.json(notes))
-      .catch((err) => res.status(500).json(err));
-  });
+  store
+    .getNotes()
+    .then((notes) => res.json(notes))
+    .catch((err) => res.status(500).json(err));
+});
 
-  //Post request for notes
-  router.post("/notes", (req, res) => {
-    store
-      .addNote(req.body)
-      .then((note) => res.json(note))
-      .catch((err) => res.status(500).json(err));
-  });
+//Post request for notes
+router.post("/notes", (req, res) => {
+  store
+    .addNote(req.body)
+    .then((note) => res.json(note))
+    .catch((err) => res.status(500).json(err));
+});
 
-  //Deletes a note by id
-  router.delete("/notes/:id", (req, res) => {
-    store
-      .removeNote(req.params.id)
-      .then(() => res.json({ ok: true }))
-      .catch((err) => res.status(500).json(err));
-  });
+//Deletes a note by id
+router.delete("/notes/:id", (req, res) => {
+  store
+    .removeNote(req.params.id)
+    .then(() => res.json({ ok: true }))
+    .catch((err) => res.status(500).json(err));
+});
 
-  module.exports = router;
+module.exports = router;
