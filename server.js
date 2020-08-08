5450941ee35750c951519b0b3634ce40922ee522
Andrew Collier <atc3598@icloud.com>
2020-08-08 13:38:34 -0400

    able to start the server

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
+
 //create port and start app;
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 //parsing and middleware
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
+
 //start server
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
